b38b6dea8f94736dde9e80e5fe7bd049abedda7c
Mario González <mario@mariogl.com>
2025-11-25 07:53:27 +0100

    chore: clean vite demo

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+import js from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            // Node built-ins
+            ["^node:"],
+            // Third-party packages
+            ["^@?\\w(?!@repo/)"],
+            // @repo/* packages
+            ["^@repo/"],
+            // Parent (..) and then sibling (.) imports
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$", "^\\."],
+            // Side effects
+            ["^\\u0000"],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+    },
+  },
+]);
