264deeffa0c017285c26cf528da24f96c8a4759f
Mario González <mario@mariogl.com>
2025-11-25 11:19:50 +0100

    feat(layout): add main navigation

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,15 +2,19 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 import AppContainer from "../ui/components/AppContainer/AppContainer";
 import MainHeader from "../ui/components/MainHeader/MainHeader";
+import Navigation from "../ui/components/Navigation/Navigation";
 import Title from "../ui/components/Title/Title";
 
 const RootComponent = () => {
   return (
     <>
       <MainHeader>
-        <Title as="text" level={2}>
+        <Title as="text" level={2} className="accent">
           Rick&Morty App
         </Title>
+        <Navigation>
+          <Navigation.Link to="/characters">Characters list</Navigation.Link>
+        </Navigation>
       </MainHeader>
       <AppContainer>
         <Outlet />
