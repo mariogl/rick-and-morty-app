ce3de4bcd9f3fa3f0679bcfcd92d4c52a6dfa6e4
Mario González <mario@mariogl.com>
2025-11-25 20:26:23 +0100

    style(characters): adjust character names' styles

--- src/characters/components/CharacterCard/CharacterCard.tsx
+++ src/characters/components/CharacterCard/CharacterCard.tsx
@@ -2,6 +2,7 @@ import Card from "@ui/components/Card/Card";
 import Title from "@ui/components/Title/Title";
 
 import type { Character } from "../../types";
+import styles from "./CharacterCard.module.css";
 
 interface CharacterCardProps {
   character: Character;
@@ -11,7 +12,9 @@ const CharacterCard = ({ character: { name } }: CharacterCardProps) => {
   return (
     <Card>
       <Card.Body>
-        <Title level={2}>{name}</Title>
+        <Title level={2} className={styles.characterName}>
+          {name}
+        </Title>
       </Card.Body>
     </Card>
   );
