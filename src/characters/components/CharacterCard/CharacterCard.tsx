11059df62523846184a1b8e1936d18cebbee2f1f
Mario González <mario@mariogl.com>
2025-11-26 13:22:28 +0100

    feat(characters): add link to character cards

--- src/characters/components/CharacterCard/CharacterCard.tsx
+++ src/characters/components/CharacterCard/CharacterCard.tsx
@@ -1,6 +1,10 @@
+import { Link } from "@tanstack/react-router";
+
 import Card from "@app/ui/components/Card/Card";
 
 import type { Character } from "../../types";
+
+import styles from "./CharacterCard.module.css";
 
 interface CharacterCardProps {
   character: Character;
@@ -10,17 +14,19 @@ const CharacterCard = ({
   character: { name, imageUrl },
 }: CharacterCardProps) => {
   return (
-    <Card type="floating">
-      <Card.Image
-        src={imageUrl}
-        alt=""
-        width={300}
-        height={300}
-        loading="lazy"
-      />
-      <Card.Body>
-        <Card.Title level={2}>{name}</Card.Title>
-      </Card.Body>
+    <Card type="floating" className={styles.character}>
+      <Link to="/characters" aria-label={`View details of ${name}`}>
+        <Card.Image
+          src={imageUrl}
+          alt=""
+          width={300}
+          height={300}
+          loading="lazy"
+        />
+        <Card.Body>
+          <Card.Title level={2}>{name}</Card.Title>
+        </Card.Body>
+      </Link>
     </Card>
   );
 };
