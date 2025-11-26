21df180db0165904a3fcf6c1a99a001ede694af3
Mario González <mario@mariogl.com>
2025-11-26 10:31:26 +0100

    style: add styles for card images

--- src/characters/components/CharacterCard/CharacterCard.tsx
+++ src/characters/components/CharacterCard/CharacterCard.tsx
@@ -14,7 +14,13 @@ const CharacterCard = ({
 }: CharacterCardProps) => {
   return (
     <Card>
-      <Card.Image src={imageUrl} alt={name} width={100} height={100} />
+      <Card.Image
+        src={imageUrl}
+        alt=""
+        width={300}
+        height={300}
+        loading="lazy"
+      />
       <Card.Body>
         <Title level={2} className={styles.characterName}>
           {name}
