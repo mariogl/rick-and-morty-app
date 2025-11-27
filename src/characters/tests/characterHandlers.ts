805bcceda4bfc9e746236db654e42da72d1ad6ce
Mario González <mario@mariogl.com>
2025-11-27 19:40:31 +0100

    test: add handler for mocking the character's detail response

--- src/characters/tests/characterHandlers.ts
+++ src/characters/tests/characterHandlers.ts
@@ -1,6 +1,7 @@
 import { http, HttpResponse } from "msw";
 
 import characterApiPaths from "@app/characters/client/characterApiPaths";
+import type { CharacterDtoPrimitives } from "@app/characters/dto/types";
 import { type CharacterListData } from "@app/characters/dto/types";
 
 import environment from "../../environment";
@@ -14,5 +15,10 @@ export const characterHandlers = [
         CharacterDtoMotherObject.createMortyDtoPrimitives(),
       ],
     });
+  }),
+  http.get(`${environment.apiBaseUrl}${characterApiPaths.characters}/1`, () => {
+    return HttpResponse.json<CharacterDtoPrimitives>(
+      CharacterDtoMotherObject.createRickDtoPrimitives(),
+    );
   }),
 ];
